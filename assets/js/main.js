adb1b0223499f42c9cc5dcbad93e422acb614275
Ariyudaaa15 <mankariyuda15@gmail.com>
2025-02-14 15:44:59 +0800

    komen pertama kali

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+const swiper = new Swiper('.logo-slider', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1024: {
+            slidesPerView: 4,
+        }
+    }
+});
+
+
